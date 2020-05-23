d25620b80746455c3e4bbacff192f142f081171c
German Gonzalez <gegonzalez@gmail.com>
2020-05-23 00:40:15 -0400

    Adding Menu and Option models

--- src/menu/models.py
+++ src/menu/models.py
@@ -1,3 +1,21 @@
+from uuid import uuid1
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    """
+    Defines the Menu data fields
+    """
+    uuid           = models.CharField(max_length=36, default="")
+    published_date = models.DateField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.uuid = uuid1()
+        super(Menu, self).save(*args, **kwargs)
+
+class Option(models.Model):
+    """
+    Defines the Option data fields
+    """
+    menu        = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
