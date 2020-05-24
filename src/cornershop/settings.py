4948b72216d1283bc0db787a12c9da92d0e859d2
German Gonzalez <gegonzalez@gmail.com>
2020-05-24 13:08:14 -0400

    Adding base.html and css in main project

--- src/cornershop/settings.py
+++ src/cornershop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'cornershop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'cornershop/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
