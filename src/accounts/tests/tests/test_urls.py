aa69de2d6c2b2558cdabbe3ebe08e1777baf3a2d
German Gonzalez <gegonzalez@gmail.com>
2020-05-24 13:01:11 -0400

    Adding Logout, redirect logic and Unit Tests for Urls

--- src/accounts/tests/tests/test_urls.py
+++ src/accounts/tests/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from accounts.views import signup_view, login_view
+from accounts.views import signup_view, login_view, logout_view
 
 class TestUrls(SimpleTestCase):
 
@@ -13,3 +13,8 @@ class TestUrls(SimpleTestCase):
         url = reverse('accounts:login')
 
         self.assertEqual(resolve(url).func, login_view)
+
+    def test_logout_url_is_resolved(self):
+        url = reverse('accounts:logout')
+
+        self.assertEqual(resolve(url).func, logout_view)
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 def signup_view(request):
     """
@@ -22,16 +22,27 @@ def signup_view(request):
 
 def login_view(request):
     """
-        Renders the login view
+        Handles the proccess to validate an user request
+        to login and renders the login view
     """
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-
-            return redirect('menu:index')
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('menu:index')
     else:
         form = AuthenticationForm()
 
     return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    """
+        Logout an user
+    """
+    if request.method == "POST":
+        logout(request)
+        return redirect('menu:index')
