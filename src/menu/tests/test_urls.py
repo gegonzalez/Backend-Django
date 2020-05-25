ad05f91dc2f0d15968d646d98fa1e7ff5b3ff6be
German <gegonzalez@gmail.com>
2020-05-25 16:55:19 -0400

    Fixing constant name

--- src/menu/tests/test_urls.py
+++ src/menu/tests/test_urls.py
@@ -4,7 +4,7 @@ from menu.views import menu_index, menu_add, menu_details_uuid, option_add, opti
 from menu.views import menu_order, order_add, order_details, order_index
 
 class TestUrls(SimpleTestCase):
-    uuid = '49653d3c-9cb8-11ea-a730-6bbac3f29050'
+    UUID = '49653d3c-9cb8-11ea-a730-6bbac3f29050'
 
     def test_index_url_is_resolved(self):
         url = reverse('menu:index')
@@ -17,32 +17,32 @@ class TestUrls(SimpleTestCase):
         self.assertEqual(resolve(url).func, menu_add)
 
     def test_details_uuid_url_is_resolved(self):
-        url = reverse('menu:details_uuid', args=[self.uuid])
+        url = reverse('menu:details_uuid', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, menu_details_uuid)
 
     def test_option_add_url_is_resolved(self):
-        url = reverse('menu:option_add', args=[self.uuid])
+        url = reverse('menu:option_add', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, option_add)
 
     def test_option_edit_url_is_resolved(self):
-        url = reverse('menu:option_edit', args=[self.uuid])
+        url = reverse('menu:option_edit', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, option_edit)
 
     def test_menu_order_url_is_resolved(self):
-        url = reverse('menu:order', args=[self.uuid])
+        url = reverse('menu:order', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, menu_order)
 
     def test_order_add_url_is_resolved(self):
-        url = reverse('menu:order_add', args=[self.uuid])
+        url = reverse('menu:order_add', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, order_add)
 
     def test_order_details_url_is_resolved(self):
-        url = reverse('menu:order_details', args=[self.uuid])
+        url = reverse('menu:order_details', args=[self.UUID])
 
         self.assertEqual(resolve(url).func, order_details)
 
